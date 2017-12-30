3cdb63b4c19a8bebc2e4d173c5f2946656bc4478
dede999 <yankees666suck>
2017-12-30 19:55:28 -0200

    schedule view changed -- simulate method's body complete

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,12 +1,20 @@
 class ScheduleController < ApplicationController
   def show
     i = 2
+    j = 2
     @schedule = Array.new(28) {[]}
     @schedule.each do |sc|
       (Match.where(:week => i)).each do |m|
-        sc << m
+        sc << m unless m.happened
       end
       i += 1
+    end
+    @played = Array.new(28) {[]}
+    @played.each do |sc|
+      (Match.where(:week => j)).each do |m|
+        sc << m if m.happened
+      end
+      j += 1
     end
   end
 end
